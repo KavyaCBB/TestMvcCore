cc6d9dee06da2cdd2fc2b9b6d2a12d7c0f089bab
kavyaBasavaraju <medha.kavya@gmail.com>
2020-08-02 17:21:40 +0530

    commit
    commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TestMvcCore.Models;
+
+namespace TestMvcCore.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/Home/About/")]
+        public IActionResult AboutUs()
+        {
+            return View("About");
+        }
+
+        [Route("/Home/Contact/")]
+        public IActionResult ContactUs()
+        {
+            return View("Contact");
+        }
+    }
+}
---
+++ Models/LoginModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace TestMvcCore.Models
+{
+    public class LoginModel
+    {
+        public int Loginid { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Contact { get; set; }
+        public string Gender { get; set; }
+        public string SocialLoginType { get; set; }
+        public string Token { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
